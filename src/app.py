0c2cf86dc8d2b86e095c391c5e749d6235a85e15
Zyro <zyrofrost@gmail.com>
2025-12-24 17:26:17 +0700

    test auth

--- src/app.py
+++ src/app.py
@@ -49,22 +49,6 @@ set_global_css()
 transaction_expander_css() # phải dùng cục bộ 1 lần, ko dùng bên view
 
 # ========= DIALOGS & USER =========
-# Login screen
-#@st.dialog(" ", dismissible=False, width = "small") # dismissible=False ẩn nút x đóng dialog
-def login_screen():
-    img = Image.open("src/assets/google_logo.png")
-    buf = io.BytesIO()
-    img.save(buf, format="PNG")
-    btn_b64 = base64.b64encode(buf.getvalue()).decode()
-
-    # load css cho icon
-    google_icon_css()
-
-    with st.container(horizontal_alignment="center"):
-        st.image("src/assets/logo.png", width=300)
-        st.markdown("<h2 style='text-align: center;'>Login to your account</h2>", unsafe_allow_html=True)
-        st.button(f'![icon](data:image/png;base64,{btn_b64})  Log in with Google', on_click=st.login, use_container_width=True)
-
 # Confirm deactivated account dialog
 @st.dialog("Account Deactivated", width="small")
 def deactivated_account_dialog():
@@ -87,6 +71,25 @@ def creating_account_dialog():
         st.rerun()
     
 # ======== USER AUTH =========
+# Login screen
+#@st.dialog(" ", dismissible=False, width = "small") # dismissible=False ẩn nút x đóng dialog
+def login_screen():
+    img = Image.open("src/assets/google_logo.png")
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    btn_b64 = base64.b64encode(buf.getvalue()).decode()
+
+    # load css cho icon
+    google_icon_css()
+    col1, col2 = st.columns([2, 1])
+    with col1.container(horizontal_alignment="center"):
+        st.image("src/assets/logo.png", width=300)
+        st.markdown("<h2 style='text-align: center;'>Login to your account</h2>", unsafe_allow_html=True)
+        if st.button(f'![icon](data:image/png;base64,{btn_b64})  Log in with Google', on_click=st.login, use_container_width=True):
+            st.login()
+    with col2.container(horizontal_alignment="center"):
+        st.image("src/assets/google_logo.png", width=50)
+
 # Check user in database, if there is no user, show login screen
 if not st.user.is_logged_in:
     login_screen()
